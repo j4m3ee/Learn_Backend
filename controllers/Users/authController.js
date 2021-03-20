c8a102a04db8d96e2d4fce4c7a7800bcb1a36ce1
j4m3ee <j4m3ee01@gmail.com>
2021-03-20 13:37:13 +0700

    fix login api error

--- controllers/Users/authController.js
+++ controllers/Users/authController.js
@@ -7,31 +7,26 @@ const checkHash = async (myText, myHash) => {
     return resultPromise
 }
 
-module.exports = async function authController(req,res){
+module.exports = async function authController(req, res) {
     const { userName, password } = req.body
     if (!userName || !password) {
         return res.status(400).send({ message: 'Please try again' })
     }
-    const user = await userModel.findOne({ userName: userName })
-    checkHash(password, user.password).then(result => {
-        if (result) {
-            const payload = {
-                id: user._id,
-                userName: user.userName
+    userModel.findOne({ userName: userName }).then(user => {
+        checkHash(password, user.password).then(result => {
+            if (result) {
+                const token = jwt.sign({
+                    id: user._id,
+                    userName: user.userName
+                }, process.env.KEY, { expiresIn: 60 * 5 }) //expire in 5 min (60sec * 5)
+                return res.send({ auth: result, token: token })
+            } else {
+                return res.send({ auth: result, message: "Password invalid" })
             }
-            const token = jwt.sign(payload, process.env.KEY, { expiresIn: 60 * 5 }) //expire in 5 min (60sec * 5)
-            const resbody = {
-                auth: result,
-                token: token
-            }
-            return res.send(resbody)
-        } else {
-            const resbody = {
-                auth: result
-            }
-            return res.send(resbody)
-        }
+        }).catch(err => {
+            return res.send({ auth: false, message: "Hash invalid." })
+        })
     }).catch(err => {
-        return res.send(err)
+        return res.send({ auth: false, message: "No user in database." })
     })
 }
