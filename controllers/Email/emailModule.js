55ded9a543d35713ec2d8f4b132e17c957f1c0a4
j4m3ee <j4m3ee01@gmail.com>
2021-03-30 21:08:58 +0700

    OAuth2

--- controllers/Email/emailModule.js
+++ controllers/Email/emailModule.js
@@ -1,11 +1,40 @@
+tls: {
+  rejectUnauthorized: false
+}
+
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENTID,
+  process.env.CLIENTSECRET,
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESHTOKEN
+});
+const accessToken = oauth2Client.getAccessToken()
 
 const transporter = nodemailer.createTransport({
-    service: process.env.MAILSERVICE,
-    auth: {
-      user: process.env.EMAILUSER, // your email
-      pass: process.env.EMAILPASSWORD // your email password
-    }
-  });
+  service: process.env.MAILSERVICE,
+  auth: {
+       type: "OAuth2",
+       user: process.env.EMAILUSER, 
+       clientId: process.env.CLIENTID,
+       clientSecret: process.env.CLIENTSECRET,
+       refreshToken: process.env.REFRESHTOKEN,
+       accessToken: accessToken
+  }
+});
+
+// const transporter = nodemailer.createTransport({
+//   service: process.env.MAILSERVICE,
+//   auth: {
+//     user: process.env.EMAILUSER, // your email
+//     pass: process.env.EMAILPASSWORD // your email password
+//   }
+// });
 
 module.exports = transporter
